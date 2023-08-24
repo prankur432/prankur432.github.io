c31bde1045f537d1fab3b53a53241b1eb04def3a
prankur432 <guptaprankur@proton.me>
2023-08-24 17:06:58 -0500

    Made Mobile Fixes

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -28,9 +28,9 @@ $(document).ready(function(){
 
   var stickySideBar = function(){
     var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper button").is(":visible");
-    // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
-    // console.log("Window Width: " + windowWidth);
-    // console.log("show: " + show);
+    console.log("has button: " + $(".author__urls-wrapper button").length === 0);
+    console.log("Window Width: " + windowWidth);
+    console.log("show: " + show);
     //old code was if($(window).width() > 1024)
     if (show) {
       // fix
